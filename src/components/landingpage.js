66306cc301d9b59fbf54382398a98a9cfa1d6f3c
Pankeel Shah <pankeelshahs@gmail.com>
2019-09-01 00:19:33 -0500

    created landing page

---
+++ src/components/landingpage.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Grid, Cell } from 'react-mdl';
+
+class Landing extends React.Component {
+    render() {
+        return (
+            <div style={{ width: '100%', margin: 'auto' }}>
+                <Grid className="landing-grid">
+                    <Cell col={12}>
+                        <img
+                            src="https://pbs.twimg.com/profile_images/831993825635745796/HnVmB0-k_400x400.jpg"
+                            alt="avatar"
+                            className="avatar-img"
+                        />
+                        <div className="banner-text">
+                            <h1>Pankeel Shah</h1>
+                            <hr />
+
+                            <p>Hi, I'm Pankeel. I'm a Computer Science student at the University of Minnesota Twin Cities.</p>
+
+                            <div className="social-links">
+
+                                {/* LinkedIn*/}
+                                <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+                                    <i className="fa fa-linkedin-square" aria-hidden="true" />
+                                </a>
+
+                                {/* GitHub*/}
+                                <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+                                    <i className="fa fa-github-square" aria-hidden="true" />
+                                </a>
+                            </div>
+                        </div>
+
+
+                    </Cell>
+                </Grid>
+            </div>
+        );
+    }
+}
+
+export default Landing;
