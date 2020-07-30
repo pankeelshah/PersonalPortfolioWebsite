33b997797ca805d9b3062edfaee16cc525109720
pankeelshah <pankeelshahs@gmail.com>
2020-07-30 00:34:44 -0500

    idk again

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react';
 import ReactGA from 'react-ga';
 import $ from 'jquery';
 import './App.css';
-import Header from './Components/Header.js';
-import Footer from './Components/Footer.js';
-import About from './Components/About.js';
-import Resume from './Components/Resume.js';
-import Contact from './Components/Contact.js';
-import Portfolio from './Components/Projects.js';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import About from './Components/About';
+import Resume from './Components/Resume';
+import Contact from './Components/Contact';
+import Portfolio from './Components/Projects';
 
 class App extends Component {
 
