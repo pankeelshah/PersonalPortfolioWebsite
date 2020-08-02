316ddb7666c482cf4e0a36c27c1d6bab72d4cf37
pankeelshah <pankeelshahs@gmail.com>
2020-08-01 20:23:38 -0500

    more changes

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -7,19 +7,22 @@ class Experience extends Component {
       var education = this.props.data.education.map(function(education){
         return <div key={education.school}><h3>{education.school}</h3>
         <p className="info">{education.degree} <span>&bull;</span><em className="date">{education.graduated}</em></p>
-        <p>{education.description}</p></div>
+        <p>{education.description}</p>
+        </div>
       })
       var work = this.props.data.work.map(function(work){
         return <div key={work.company}><h3>{work.company}</h3>
             <p className="info">{work.title}<span>&bull;</span> <em className="date">{work.years}</em></p>
-            <p>{work.description}</p>
+            <p><span>&bull;</span> {work.description}</p>
+            <p><span>&bull;</span> {work.description2}</p>
+            <p><span>&bull;</span> {work.description3}</p>
         </div>
       })
       // var skills = this.props.data.skills.map(function(skills){
       //   var className = 'bar-expand '+skills.name.toLowerCase();
       //   return <li key={skills.name}><span style={{width:skills.level}}className={className}></span><em>{skills.name}</em></li>
       // })
-      var classes = this.props.data.classes.map(element => <li>{element}</li>)
+      var classes = this.props.data.classes.map(element => <li><span>&bull;</span> {element}</li>)
       
     }
 
